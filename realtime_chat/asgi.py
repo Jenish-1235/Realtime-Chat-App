5fe065a645a8199ca33e9e0d05ff8fcb6a6244c1
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-01-15 05:06:47 +0530

    create-chat-app

--- realtime_chat/asgi.py
+++ realtime_chat/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for realtime_chat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
+import django
+from channels.routing import get_default_application
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtime_chat.settings')
+django.setup()
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing  # (We’ll create chat.routing later)
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns  # We'll define this soon
+        )
+    ),
+})
